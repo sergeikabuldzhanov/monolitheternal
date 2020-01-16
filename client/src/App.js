02cb27da58f58e6115b06684f0e00a77507a8306
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-16 15:54:33 +0500

    Hitting my own endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(()=>{
+    fetch(`/api/greeting?name=Gabe`)
+      .then(res=>res.json)
+      .then(greeting=>setGreeting(greeting))
+      .catch(error=>{
+        console.log(error);
+        
+      })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{greeting}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
